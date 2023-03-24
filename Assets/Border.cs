43052cd3a201c5c1e8325f06e699d612888c4eed
DESKTOP-AO5DABD\ryan <ryanenclona@gmail.com>
2023-03-24 22:22:14 +0800

    made border and wall classes

---
+++ Assets/Border.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    [SerializeField] private Wall LeftWall;
+    [SerializeField] private Wall RightWall;
+    [SerializeField] private Wall TopWall;
+    [SerializeField] private Wall BottomWall;
+
+    [SerializeField] private Camera MainCamera;
+
+    private void Awake()
+    {
+        SetupWall();
+    }
+
+    private void SetupWall()
+    {
+        LeftWall.transform.position = GetScreenEdgePosition(Vector2.left);
+        RightWall.transform.position = GetScreenEdgePosition(Vector2.right);
+        TopWall.transform.position = GetScreenEdgePosition(Vector2.up);
+        BottomWall.transform.position = GetScreenEdgePosition(Vector2.down);
+
+    }
+
+    private Vector2 GetScreenEdgePosition(Vector2 screenEdge)
+    {
+        if (screenEdge == Vector2.up)
+        {
+            return MainCamera.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height, 0));
+        }
+
+        if (screenEdge == Vector2.down)
+        {
+            return MainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0));
+        }
+
+        if (screenEdge == Vector2.right)
+        {
+            return MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height/2, 0));
+        }
+
+        if (screenEdge == Vector2.left)
+        {
+            return MainCamera.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, 0));
+        }
+
+        return Vector2.zero;
+    }
+}
