cc10c0ade3cd2674fa0764f0081bfc54688549f9
DESKTOP-AO5DABD\ryan <ryanenclona@gmail.com>
2023-03-26 17:07:56 +0800

    moved theass ClientSendParamsCache to its own script

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -91,39 +91,3 @@ public class PlayerSpawner : NetworkBehaviour
         return Vector3.zero;
     }
 }
-
-public static class ClientSendParamsCache
-{
-    private static Dictionary<ulong, ClientRpcParams> ClientParams = new Dictionary<ulong, ClientRpcParams>();
-
-    public static void RegisterClientForParams(ulong clientID, ClientRpcParams param)
-    {
-        if (ClientParams.ContainsKey(clientID))
-        {
-            return;
-        }
-        ClientParams.Add(clientID, param);
-    }
-
-    public static ClientRpcParams GetSendParamsForClient(ulong clientID)
-    {
-        if (ClientParams.ContainsKey(clientID))
-        {
-            return ClientParams[clientID];
-        }
-
-        ClientRpcParams newParams = new ClientRpcParams
-        {
-            Send = new ClientRpcSendParams
-            {
-                TargetClientIds = new ulong[]
-                {
-                    clientID
-                }
-            }
-        };
-        ClientParams.Add(clientID, newParams);
-
-        return newParams;
-    }
-}
