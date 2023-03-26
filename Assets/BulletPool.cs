c4a699697d834e87dc3f68e9028d32b5c17889f7
DESKTOP-AO5DABD\ryan <ryanenclona@gmail.com>
2023-03-26 18:27:04 +0800

    Move VectorangleHelper Class to its own file

--- Assets/BulletPool.cs
+++ Assets/BulletPool.cs
@@ -6,12 +6,3 @@ public class BulletPool : MonoBehaviour
 {
    
 }
-
-public static class VectorAngleHelper
-{
-    public static Vector2 AngleToVector2(float angle)
-    {
-        angle *= Mathf.Deg2Rad;
-        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-    }
-}
---
+++ Assets/VectorAngleHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class VectorAngleHelper
+{
+    public static Vector2 AngleToVector2(float angle)
+    {
+        angle *= Mathf.Deg2Rad;
+        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+}
